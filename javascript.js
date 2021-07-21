6e5e9e3fb3088a35e0bec493815dc5a11d6123d0
ecleaver <erincleaver@grcc.email.edu>
2021-07-20 23:09:39 -0400

    push

--- javascript.js
+++ javascript.js
@@ -9,15 +9,63 @@ function getCookie(name) {
     return null;
 }
 
+function setCookie(name, value) {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() +1);
+    document.cookie = `${name}=${value} expires="${date.toGMTString()}"`;
+}
+
+let btnAcceptCookies = document.getElementById('btn-accept-cookies');
+let overlay = document.getElementById('overlay');
+let wrapper = document.querySelector('.wrapper');
+btnAcceptCookies.addEventListener('click', (ev)=>{
+    document.cookie = "cookieAgreement=greenbackend.com; max-age=" + 60*60*24*30;
+    if(document.cookie) {
+        overlay.style.display = 'none';
+        wrapper.style.display = 'none';
+    } else {
+        alert("Cookie can't be sent! Please unlock this site from the cookie settings in your borwser.")
+    }
+})
+
+overlay.addEventListener('click', (ev)=>{
+
+    overlay.style.display = 'none';
+    wrapper.style.display = 'none';
+   
+})
+
+let = checkCookie = document.cookie.indexOf("cookieAgreement");
+
+if(checkCookie != -1) {
+    overlay.style.display = 'none';
+    wrapper.style.display = 'none';
+} else {
+    overlay.style.display = 'block';
+    wrapper.style.display = 'flex';
+}
+
+
+
+
 class Settings {
     constructor() {
         this.menu_continer = document.querySelector(".menu_continer");
+        this.overlay = document.getElementById('overlay');
         this.pomodoro = 25;
         this.short_breake = 1;
         this.long_break = 15;
         this.setTimer(this.pomodoro, this.short_breake, this.long_break);
     }
+    
+    clickOverlay() {
+        this.overlay.addEventListener('click', (ev)=>{
 
+            this.overlay.style.display = 'none';
+            this.menu_continer.style.display = 'none';
+           
+        })
+    }
     
 
     accessSettings () {
@@ -25,6 +73,7 @@ class Settings {
         settings_button.addEventListener("click", (ev) => {
             ev.preventDefault();
             this.menu_continer.style.display = 'flex';
+            this.overlay.style.display = 'block';
         }) 
     }
 
@@ -38,6 +87,7 @@ class Settings {
             console.log(this.pomodoro)
             this.setTimer(this.pomodoro, this.short_breake, this.long_break);
             this.menu_continer.style.display = 'none';
+            this.overlay.style.display = 'none';
         })
     }
 
@@ -72,24 +122,37 @@ class Timer {
         this.dashoffset = 1024;
         this.offset;
         this.timerCircle = document.getElementById('timer_circle');
-        
+        this.pomodoro_button = document.getElementById('pomodoro')
+        this.short_breake_button= document.getElementById("short_breake");
+        this.long_break_button = document.getElementById("long_break_button");
     }
 
     
 
     loadButtons(settings) {
+        console.log(getCookie("buttonSelected"))
+
+
         this.pomodoroButton(settings);
         this.shortBreakeButton(settings);
         this.longBreakButton(settings);
+        if(getCookie("buttonSelected")[0] == 0) {
+            this.pomodoro_button.click();
+        } else if(getCookie("buttonSelected")[0] == 1) {
+            this.short_breake_button.click();
+        } else if(getCookie("buttonSelected")[0] == 2) {
+            this.long_break_button.click();
+        }
+        
     }
 
     pomodoroButton(settings) {
-        let pomodoro = document.getElementById("pomodoro_button");
-        pomodoro.addEventListener("click", (ev) => {
+        pomodoro_button.addEventListener("click", (ev) => {
             ev.preventDefault();
             this.currentTime(settings.pomodoro, "00");
-            
+            setCookie("buttonSelected", 0);
             this.selectedMode = settings.pomodoro;
+            this.dashoffset = 1024;
             this.buttonSilder('0%');
         })
     }
@@ -99,21 +162,26 @@ class Timer {
     }
 
     shortBreakeButton(settings) {
-        let short_breake = document.getElementById("short_breake_button");
-        short_breake.addEventListener("click", (ev) => {
+        this.short_breake_button= document.getElementById("short_breake_button");
+        this.short_breake_button.addEventListener("click", (ev) => {
             this.currentTime(settings.short_breake, "00");
+            setCookie("buttonSelected", 1);
             this.selectedMode = settings.short_breake;
+            this.dashoffset = 1024;
             ev.preventDefault();
             this.buttonSilder('33%');
         })
     }
 
     longBreakButton() {
-        let long_break = document.getElementById("long_break_button");
-        long_break.addEventListener("click", (ev) => {
+        
+        this.long_break_button.addEventListener("click", (ev) => {
             ev.preventDefault();
+            setCookie("buttonSelected", 2);
             this.currentTime(settings.long_break, "00");
             this.selectedMode = settings.long_break;
+            setCookie("selectedMode", this.selectedMode);
+            this.dashoffset = 1024;
             this.buttonSilder('66%');
         })
     }
@@ -131,13 +199,14 @@ class Timer {
                 this.dashoffset = parseInt(getCookie("lastOffset"));
                 console.log(this.dashoffset);
                 this.currentSecounds = parseInt(getCookie("lastTime"));
+                this.totalSecounds = parseInt(getCookie("totalSecounds"));
                 this.mintues = Math.floor(this.currentSecounds/60);
                 this.secounds = this.currentSecounds%60;
                 this.currentTime(this.getTimeString(this.mintues), this.getTimeString(this.secounds));
                 this.timerCircle.setAttribute("stroke-dashoffset", this.dashoffset);
-                document.cookie = "lastOffset=; expires 19 Jun 2021 00:00:00 UTC";
-
             } 
+
+            
 
         document.addEventListener("click", (ev) => {
             ev.preventDefault();
@@ -176,7 +245,6 @@ class Timer {
           this.currentSecounds = this.totalSecounds;
         }  
     
-        console.log("Checking for nan:" + this.dashoffset);
 
         
         this.timerCircle.setAttribute("stroke-dashoffset", this.dashoffset)
@@ -195,19 +263,19 @@ class Timer {
             
             let precent =((this.currentSecounds%this.totalSecounds)/this.totalSecounds)*100;
 
-            console.log("Dashoffset: " + Math.ceil((precent/100) * 1024));
+            console.log("Dashoffset: " + this.dashoffset);
             this.dashoffset = Math.ceil((precent/100) * 1024);
             
-            console.log(this.currentSecounds)
+            // console.log(this.currentSecounds)
+            console.log("Value listed: " + this.dashoffset)
             this.timerCircle.setAttribute("stroke-dashoffset", this.dashoffset);
-            console.log("time circle: " + this.timerCircle.style.strokeDashoffset)
 
 
             this.currentTime(this.getTimeString(this.mintues), this.getTimeString(this.secounds));
-            document.cookie = "lastOffset="+this.dashoffset;
-            document.cookie = "lastTime="+this.currentSecounds;
+            setCookie("lastOffset", this.dashoffset)
+            setCookie("lastTime", this.currentSecounds)
+            setCookie("totalSecounds", this.totalSecounds)
 
-            console.log(this.dashoffset)
             if(this.getStopTimmer()) {
                 clearInterval(timer);
                 this.stopTimer = false;
@@ -250,6 +318,7 @@ settings = new Settings();
 
 settings.accessSettings();
 settings.applyChanges();
+settings.clickOverlay();
 timer = new Timer(settings);
 timer.loadButtons(settings);
 timer.timer();
